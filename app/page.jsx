2556cd2ff4f0603682a8da91f2918fcdeea343b0
vishesh agarwal <136541738+vishesh-1-bee@users.noreply.github.com>
2025-10-29 23:13:53 +0530

    Updated theams

---
+++ app/page.jsx
@@ -0,0 +1,18 @@
+"use client"
+import { Button } from '@/components/ui/button'
+import { useTheme } from 'next-themes'
+import React from 'react'
+
+const page = () => {
+  const { setTheme } = useTheme()
+  return (
+    <div>
+      <h1 >we are here</h1> 
+    
+     <Button onClick={()=>setTheme('light')}  >Light Mode</Button>
+     <Button onClick={()=>setTheme('dark')} >Dark Mode</Button>
+    </div>
+  )
+}
+
+export default page
---
+++ app/provider.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+const Provider = ({
+    children,
+    ...props
+}) => {
+    return (
+        <NextThemesProvider 
+         attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+        {...props}>
+           <div>{children}</div>
+        </NextThemesProvider>
+    )
+}
+
+export default Provider
